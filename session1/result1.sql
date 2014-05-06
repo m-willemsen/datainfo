45bccb0d0611b901e56e8665a52237afba8b460b
m-willemsen <m.j.willemsen@student.utwente.nl>
2014-05-06 14:36:48 +0200

    Opdracht 1

---
+++ session1/result1.sql
@@ -0,0 +1,11 @@
+INSERT INTO Problem1 (`task`, `result`, `motivation`) VALUES 
+("a", false, "Er zijn wellicht meerdere voertuigen met dezelfde capaciteit"), 
+("b", true, "Van elk voertuig is bekend wat de capaciteit is"),
+("c", true, "Je weet door F wat P is en zelfs A alleen was voldoende geweest"),
+("d", true, "Er staat dat er per pakketje één factuur is, en door F weet je A"),
+("e", false, "Van P is alleen O bekend en van O weet je alleen P"),
+("f", false, "Een bezorger B kan voor meerdere voertuigen V gerechtigd zijn"),
+("g", false, "Voor een voertuig met een bepaalde capaciteit kunnen meerdere B's gerechtigd zijn"),
+("h", true, "Door de dubbele pijl mogen er meerdere B's zijn voor die ene V"),
+("i", true, "Een voertuig heeft een bepaalde capaciteit"),
+("j", true, "Dit is een combinatie van de bovenste en die zijn allebei true");
