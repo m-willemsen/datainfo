817e69e64f93ec63a8bd952ab2879ffc19d28408
m-willemsen <m.j.willemsen@student.utwente.nl>
2014-05-06 16:32:15 +0200

    Opdracht 3 - shunting rule en DISTINCT weggelaten

--- session1/result3d.sql
+++ session1/result3d.sql
@@ -1,8 +1,6 @@
-SELECT DISTINCT p.name FROM person p, Writes w WHERE w.pid=p.pid AND NOT EXISTS (
+SELECT p.name FROM person p, Writes w WHERE w.pid=p.pid AND NOT EXISTS (
 	SELECT * FROM Directs d WHERE d.mid = w.mid
 ) AND NOT EXISTS (
-	SELECT * FROM Writes w WHERE w.pid = p.pid AND EXISTS (
-		SELECT * FROM Directs d WHERE d.mid = w.mid
-	)
+	SELECT * FROM Writes w, Directs d WHERE w.pid = p.pid AND d.mid = w.mid
 );
 
