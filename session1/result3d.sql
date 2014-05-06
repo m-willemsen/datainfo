b8b7db60a68a8eba4da2492c4f03be20dd7898d3
m-willemsen <m.j.willemsen@student.utwente.nl>
2014-05-06 16:29:52 +0200

    Opdracht 3d - basis van 3c een extra eis toegevoegd in where

---
+++ session1/result3d.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT p.name FROM person p, Writes w WHERE w.pid=p.pid AND NOT EXISTS (
+	SELECT * FROM Directs d WHERE d.mid = w.mid
+) AND NOT EXISTS (
+	SELECT * FROM Writes w WHERE w.pid = p.pid AND EXISTS (
+		SELECT * FROM Directs d WHERE d.mid = w.mid
+	)
+);
+
